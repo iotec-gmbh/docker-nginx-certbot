4fa579290034337ad50946d9d327a9a8fc573402
Lars Kiesow <lkiesow@uos.de>
2018-07-03 23:53:26 +0200

    get certificate

--- run.sh
+++ run.sh
@@ -11,8 +11,12 @@ CF_CONFIG_FILE=/etc/letsencrypt/dnscloudflare.ini
 echo "dns_cloudflare_api_key = ${CLOUDFLARE_API_KEY}" > "${CF_CONFIG_FILE}"
 echo "dns_cloudflare_email = ${CLOUDFLARE_ACCOUNT}" > "${CF_CONFIG_FILE}"
 
+# Get certificate
+# TODO: In background. Daily, one week before expiration
+certbot certonly -d "*.${DOMAIN}" --dns-cloudflare
+
 # Launch nginx
-CERT=/etc/letsencrypt/live/kiesow.be/fullchain.pem
-KEY=/etc/letsencrypt/live/kiesow.be/privkey.pem
+CERT="/etc/letsencrypt/live/${DOMAIN}/fullchain.pem"
+KEY="/etc/letsencrypt/live/${DOMAIN}/privkey.pem"
 while [ ! -f "${CERT}" ]; do sleep 1; done
 nginx -g "daemon off;"
